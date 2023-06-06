aa663bf979638c1fdd812e8d93d21aea86441c02
damara <damarmuhamad13@gmail.com>
2023-06-06 11:11:47 +0700

    update-pokemon.js

--- pokemon/pokemon.js
+++ pokemon/pokemon.js
@@ -1,17 +1,17 @@
-const container = document.querySelector('#container');
-const baseImgURL ='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const container = document.querySelector('#container'); // mengambil elemen dg .querySelector
+const baseImgURL ='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'; 
 
 for (let i = 1; i <= 1000; i++) {
-	const pokeBall = document.createElement('div');
-	pokeBall.classList.add('pokemon');
+	const pokeBall = document.createElement('div'); // membuat element baru
+	pokeBall.classList.add('pokemon'); // menambah class
 
 	const imgPokemon = document.createElement('img');
 	imgPokemon.src = `${baseImgURL}${i}.png`;
 
 	const label = document.createElement('span');
-	label.innerText = i;
+	label.innerText = i; // innerText untuk menampilkan dalam html
 
-	pokeBall.appendChild(imgPokemon);
+	pokeBall.appendChild(imgPokemon); // menambahkan elemen sebagai anak terakhir dari elemen.
 	pokeBall.appendChild(label);
 	container.appendChild(pokeBall);
 }
