de99b999ca5c46be984f071586951e5c61c4e775
damara <damarmuhamad13@gmail.com>
2023-06-06 11:06:20 +0700

    update-main.js

--- changecolor/main.js
+++ changecolor/main.js
@@ -1,14 +1,14 @@
-const button = document.querySelector('button'); // 
-const judul = document.querySelector('h1');
+const button = document.querySelector('button'); // mengambil elemen dg .querySelector
+const judul = document.querySelector('h1'); // mengambil elemen dg .querySelector
 
-const generateColor = () => {
-    const r = Math.floor(Math.random() * 255);
+const generateColor = () => { // arrow function untuk generate color 
+    const r = Math.floor(Math.random() * 255); // pengkalian dg math random agar warna terus berubah
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
     return `rgb(${r}, ${g}, ${b})`;
 };
 
-button.addEventListener('click', () =>  {
+button.addEventListener('click', () =>  { // add event list pada button 
     const newColor = generateColor();
     document.body.style.backgroundColor = newColor;
     judul.innerText = newColor;
