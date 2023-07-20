0c11e6d2d21f8fe56d072564bc1a4d9293ee3af0
damara <damarmuhamad13@gmail.com>
2023-07-20 15:16:05 +0700

    update: add-keyword-await

--- asyncronous/backgroundchanger/app.js
+++ asyncronous/backgroundchanger/app.js
@@ -7,11 +7,18 @@ const delayedColorChange = (color, delay) => {
     })
 }
 
-delayedColorChange('red', 1000)
-    .then(() => delayedColorChange('yellow', 1000))
-    .then(() => delayedColorChange('green', 1000))
-    .then(() => delayedColorChange('blue', 1000))
-    .then(() => delayedColorChange('purple', 1000))
-    .then(() => delayedColorChange('orange', 1000))
-    .then(() => delayedColorChange('white', 1000))
-    .then(() => delayedColorChange('black', 1000))
+// delayedColorChange('red', 1000)
+//     .then(() => delayedColorChange('yellow', 1000))
+//     .then(() => delayedColorChange('green', 1000))
+//     .then(() => delayedColorChange('blue', 1000))
+//     .then(() => delayedColorChange('purple', 1000))
+//     .then(() => delayedColorChange('orange', 1000))
+//     .then(() => delayedColorChange('white', 1000))
+//     .then(() => delayedColorChange('black', 1000))
+
+async function changeColor() {
+    delayedColorChange('red', 1000);
+    delayedColorChange('yellow', 1000)
+}
+
+changeColor();
