0d0c0ba32e794372d6d70450b63395b0bacae260
damara <damarmuhamad13@gmail.com>
2023-07-22 09:18:24 +0700

    update: add-ajax-json

--- ajax-and-api/ajaj/app.js
+++ ajax-and-api/ajaj/app.js
@@ -1,5 +1,2 @@
-const data = `{
-    "ticker" : {
-        "base": "BTC"
-    }
-}`
+// untuk membaca json pada java script maka bisa menggunakan JSON.parse()
+// untuk mengirim ke server maka bisa gunakan JSON.stringify()
