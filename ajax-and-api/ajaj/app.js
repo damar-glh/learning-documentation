61bb73b842b490a304017082b9999d93fd422645
damara <damarmuhamad13@gmail.com>
2023-09-07 18:05:40 +0700

    update: request-with-fetch

--- ajax-and-api/ajaj/app.js
+++ ajax-and-api/ajaj/app.js
@@ -3,46 +3,46 @@
 const req = new XMLHttpRequest();
 let data;
 
-// req.onload = function () {
-// 	data = JSON.parse(this.responseText);
-// 	console.log(data);
-// };
+req.onload = function () {
+	data = JSON.parse(this.responseText);
+	console.log(data);
+};
 
-// req.onerror = function () {
-// 	console.log('Error', this);
-// };
+req.onerror = function () {
+	console.log('Error', this);
+};
 
-// req.open('GET', 'https://swapi.dev/api/people/3', true);
-// myReq.setRequestHeader('Accept', 'application/json');
-// req.send();
+req.open('GET', 'https://swapi.dev/api/people/3', true);
+myReq.setRequestHeader('Accept', 'application/json');
+req.send();
 
-// fetch('https://swapi.dev/api/people/1')
-// 	.then((res) => {
-// 		if (!res.ok) {
-// 			throw Error('Could not fetch the data for that resource');
-// 		}
-// 		return res.json();
-// 	})
-// 	.then((data) => {
-// 		console.log('json 1', data);
-// 		return fetch('https://swapi.dev/api/people/2');
-// 	})
-// 	.then((res) => {
-// 		console.log('request 2');
-// 		if (!res.ok) {
-// 			throw Error('Could not fetch the data for that resource');
-// 		}
-// 		return res.json();
-// 	})
-// 	.then((data) => {
-// 		console.log(data);
-// 	})
-// 	.catch((err) => {
-// 		console.log('error', err);
-// 	});
+fetch('https://swapi.dev/api/people/1')
+	.then((res) => {
+		if (!res.ok) {
+			throw Error('Could not fetch the data for that resource');
+		}
+		return res.json();
+	})
+	.then((data) => {
+		console.log('json 1', data);
+		return fetch('https://swapi.dev/api/people/2');
+	})
+	.then((res) => {
+		console.log('request 2');
+		if (!res.ok) {
+			throw Error('Could not fetch the data for that resource');
+		}
+		return res.json();
+	})
+	.then((data) => {
+		console.log(data);
+	})
+	.catch((err) => {
+		console.log('error', err);
+	});
 
 
-// request with fetch
+// simple request with fetch
 const loadPeople = async () => {
 	try {
 		const res = await fetch ('https://swapi.dev/api/people/3');
