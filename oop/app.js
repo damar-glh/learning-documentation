9c74c17dd23734f6a8d5225b71dcb345e8bf6321
damara <damarmuhamad13@gmail.com>
2023-09-19 15:12:50 +0700

    update:factory-function-folder

--- oop/app.js
+++ oop/app.js
@@ -1,26 +1,3 @@
-// factory function = function yg tidak membutuhkan keyword this / new dalam pembuatan object
-// function bisa langsung dipanggil melalui object yang dibuat
-function changeColor(r, g, b) {
-    const color = {}; // create object color
-
-    // add rgb from object
-    color.r = r;
-    color.g = g;
-    color.b = b;
-
-    // function rgb from object
-    color.rgb = function () {
-        const {r, g, b} = this;
-        return `rgb(${r}, ${g}, ${b})`;
-    }
-
-    // function hex from object
-    color.hex = function(){
-        const {r, g, b} = this;
-        return "#" + ((1 << 24) + (r << 16) + (g << 8) +b).toString(16).slice(1);
-    }
-};
-
 // buat object dengan constructor function
 // contoh constructor function dengan nama function diawali huruf besar 
 function Color(r, g, b) {
