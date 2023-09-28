7a0272f7e43e8efa82459e6e35da28a9d8eb6b04
damara <damarmuhamad13@gmail.com>
2023-09-28 21:12:00 +0700

    add: binding-data-in-vuejs

--- vuejs/vuejs-learning/app.js
+++ vuejs/vuejs-learning/app.js
@@ -2,7 +2,7 @@ let product = {
     name: "ROG Zephyrus G14 GA401",
     description: "Styled in metallic Eclipse Gray or pearlescent Moonlight White, the Zephyrus G14 is a stunner. Its clean aesthetic travels easily from LAN parties to lecture halls, with fine-tuned detailing that puts it in a class of its own. Precision CNC milling creates 6,536 perforations across the aluminum lid for a distinctive dot matrix design. Honeycomb reinforcements add strength with minimal material to keep the chassis light. The magnesium-aluminum alloy deck is fingerprint resistant to keep it looking fresh day to day.",
     price: "60.000.000",
-    image: "OIP.jpeg",
+    image: "https://th.bing.com/th/id/OIP.iUPhRErHJhGA7zhaA9JONQHaE8?pid=ImgDet&rs=1",
 }
 
 let app = new Vue({
