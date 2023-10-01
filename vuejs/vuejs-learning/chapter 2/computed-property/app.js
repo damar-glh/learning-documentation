571be672d4b90a9422f991640321a5a63252292f
damara <damarmuhamad13@gmail.com>
2023-10-02 06:11:05 +0700

    add(computed-method): difference between methods and computed properties

--- vuejs/vuejs-learning/chapter 2/computed-property/app.js
+++ vuejs/vuejs-learning/chapter 2/computed-property/app.js
@@ -4,11 +4,35 @@ let app = new Vue({
         slugText: 'The Quick #(*#&@ Brown 29',
     },
     computed: {
+        // jika now computed properties now maka nilainya tidak dinamis dan 
+        // nilainya tidak berubah/tidak dinamis, 
+        // datanya tidak diubah terus menerus
+        // now: function() {
+        //     const date = new Date();
+        //     return (
+        //         String(date.getSeconds()) +
+        //         String(date.getMinutes()) +
+        //         String(date.getHours())
+        //     );
+        // },
         slugetize: function() {
             return this.slugText
             .toLowerCase()
             .replace(/[^\w ]+/g, '')
-            .replace(/ +/g, '-')
+            .replace(/ +/g, '-')+ '-' + this.now();
+        },
+    },
+    methods: {
+        // sedangkan now di methods nilainya dinamis 
+        // dan nilainya berubah terus menerus 
+        // datanya disimpan dan diubah terus menerus
+        now: function() {
+            const date = new Date();
+            return (
+                String(date.getSeconds()) +
+                String(date.getMinutes()) +
+                String(date.getHours())
+            );
         },
     },
 })
