bf15e9962796aedaad029dec48878c4c39dace1c
damara <damarmuhamad13@gmail.com>
2023-11-05 14:13:42 +0700

    add(): Data Filter Features and useState Introduction

--- react-fundamental/src/pages/index.jsx
+++ react-fundamental/src/pages/index.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import posts from '../posts.json'
 import Article from '../components/Article'
 
 function HomePage(){
+    const [search, setSearch] = useState("")
+
+    const changeSearch = (event) => {
+        setSearch(event.target.value);
+    }
+
     return (
         <>
             <h1>Simple Blog</h1>
-            {posts.map((blog) => (
-                <Article key={blog.id} id={blog.id} title={blog.title} tags={blog.tags} date={blog.date} />
+            <div className="">Cari Artikel : <input type="text" onChange={changeSearch} />{" "}</div>
+            <small>Data yang anda cari adalah {search}</small>
+            {posts.map(({id, title, tags, date}) => (
+                <Article key={id} {...{id, title, tags, date}} />
             ))}
         </>
     )
