2b75301e6b451d7263fa8918810c4c3baa8790d0
Damar Galih <damarmuhamad13@gmail.com>
2024-09-27 21:43:26 +0700

    feat: make counter app with options api vue

---
+++ vuejs/counter-vue-options-api/src/App.vue
@@ -0,0 +1,75 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1>Count Counter</h1>
+      <span id="value">
+        {{ count }}
+      </span>
+      <div>
+        <button @click="increment" id="increment">Increment</button>
+        <button @click="decrement" id="decrement">Decrement</button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  text-align: center;
+  background-color: #333333;
+  padding: 20px;
+  border-radius: 8px;
+}
+
+#value {
+  font-size: 2rem;
+  color: #ffffff;
+}
+
+button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 8px;
+}
+
+button:hover {
+  background-color: #45a049;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
