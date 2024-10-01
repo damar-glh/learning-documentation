510d55c7a27cf2e4ba9a5d53887baaac5d8691ff
Damar Galih <damarmuhamad13@gmail.com>
2024-10-01 08:00:18 +0700

    feat: creating memo section ui

--- vuejs/stickynotes-vue/src/App.vue
+++ vuejs/stickynotes-vue/src/App.vue
@@ -9,7 +9,18 @@
         <h1 class="header-title">Memo</h1>
         <button class="header-button">add +</button>
       </header>
-      <div>[card]</div>
+      <div class="card-container">
+        <div class="card">
+          <p class="card-content">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.
+          </p>
+          <p class="card-date">
+            12/12/2024
+          </p>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -47,5 +58,22 @@ header {
   border-radius: 20px;
   background-color: #495a7d;
   color: white;
+}
+
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+
+.card {
+  width: 225px;
+  height: 225px;
+  padding: 10px;
+  background-color: #ffa6c1;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 </style>
