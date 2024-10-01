1c441859081a650ca6461647c1e4bb04e0ace7c1
Damar Galih <damarmuhamad13@gmail.com>
2024-10-01 07:49:51 +0700

    feat: add stickynotes-vue

---
+++ vuejs/stickynotes-vue/src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <header>
+        <h1 class="header-title">Memo</h1>
+        <button class="header-button">add +</button>
+      </header>
+      <div>[card]</div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  height: 100vh;
+  width: 100vh;
+}
+
+.container {
+  max-width: 900px;
+  padding: 10px;
+  margin: 0 auto;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header-title {
+  font-size: 48px;
+  font-weight: bold;
+  margin-bottom: 25px;
+  color: #495a7d;
+}
+
+.header-button {
+  border: none;
+  padding: 10px;
+  width: 100px;
+  cursor: pointer;
+  border-radius: 20px;
+  background-color: #495a7d;
+  color: white;
+}
+</style>
