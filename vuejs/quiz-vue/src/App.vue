2c8065b1d632f91317794bf590f331eaaba7421d
Damar Galih <damarmuhamad13@gmail.com>
2024-10-14 14:12:16 +0700

    ins: quiz vue

---
+++ vuejs/quiz-vue/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <header>
+      <h1 id="title">Quiz Vue</h1>
+      <input type="text" name="" id="search-input">
+    </header>
+  </main>
+</template>
+<style scoped>
+main {
+  max-width: 900px;
+  margin: 0 auto;
+}
+
+header {
+  margin: 30px 0 10px 0;
+  display: flex;
+  align-items: center;
+}
+
+#title {
+  margin-right: 20px;
+  font-weight: bold;
+}
+
+#search-input {
+  border: none;
+  padding: 10px;
+  background-color: whitesmoke;
+  border-radius: 5px;
+}
+</style>
