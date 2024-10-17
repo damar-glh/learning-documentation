68c068f2cef647016242acf4d2f88a6c5083aaf2
Damar Galih <damarmuhamad13@gmail.com>
2024-10-17 14:57:37 +0700

    feat: search filtering quiz

--- vuejs/quiz-vue/src/App.vue
+++ vuejs/quiz-vue/src/App.vue
@@ -1,18 +1,35 @@
 <script setup>
-import { ref } from 'vue';
+import {ref, watch} from 'vue';
 import srcQiz from './data/quizes.json';
 
 const quizes = ref(srcQiz);
+const searchInput = ref('');
+const notFound = ref(false);
+
+watch(searchInput, (newValue) => {
+  if (newValue === '') {
+    quizes.value = srcQiz;
+    notFound.value = false;
+  } else {
+    quizes.value = srcQiz.filter((quiz) => {
+      return quiz.title.toLowerCase().includes(newValue.toLowerCase());
+    });
+  }
+  notFound.value = quizes.value.length === 0;
+});
 </script>
 
 <template>
   <main>
     <header>
       <h1 id="title">Quiz Vue</h1>
-      <input type="text" name="" id="search-input">
+      <input type="text" name="" id="search-input" v-model.trim="searchInput"/>
     </header>
     <section id="quiz-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+      <div v-if="notFound">
+        <p>Topik quiz Not Found!!</p>
+      </div>
+      <div v-else v-for="quiz in quizes" :key="quiz.id" class="card">
         <img :src="quiz.img" :alt="quiz.title">
         <div class="card-body">
           <h2>{{ quiz.title }}</h2>
