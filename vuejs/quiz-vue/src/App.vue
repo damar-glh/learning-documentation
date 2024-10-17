503e12cda706416ad0863aa59704bac0e33963ba
Damar Galih <damarmuhamad13@gmail.com>
2024-10-17 11:00:50 +0700

    fea: ui for quiz vue

--- vuejs/quiz-vue/src/App.vue
+++ vuejs/quiz-vue/src/App.vue
@@ -4,6 +4,16 @@
       <h1 id="title">Quiz Vue</h1>
       <input type="text" name="" id="search-input">
     </header>
+    <section id="quiz-container">
+      <div class="card">
+        <img src="" alt="" srcset="">
+        <div class="card-body">
+          <h2>Question</h2>
+          <p>Answer</p>
+          <button>Next</button>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 <style scoped>
@@ -28,5 +38,35 @@ header {
   padding: 10px;
   background-color: whitesmoke;
   border-radius: 5px;
+}
+
+#quiz-container {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin-top: 20px;
+}
+
+.card {
+  width: 300px;
+  margin: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+}
+
+.card img {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+}
+
+.card-body {
+  padding: 0 10px;
+}
+
+h2 {
+  font-weight: bold;
 }
 </style>
