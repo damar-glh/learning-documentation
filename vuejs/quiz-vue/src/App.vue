cc78f6267b18a8206ffa09b5d62cb37ecb037c92
Damar Galih <damarmuhamad13@gmail.com>
2024-10-19 16:22:15 +0700

    feat(quizcard): separating own components

--- vuejs/quiz-vue/src/App.vue
+++ vuejs/quiz-vue/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {ref, watch} from 'vue';
 import srcQiz from './data/quizes.json';
+import QuizCard from './components/QuizCard.vue';
 
 const quizes = ref(srcQiz);
 const searchInput = ref('');
@@ -29,14 +30,7 @@ watch(searchInput, (newValue) => {
       <div v-if="notFound">
         <p>Topik quiz Not Found!!</p>
       </div>
-      <div v-else v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img :src="quiz.img" :alt="quiz.title">
-        <div class="card-body">
-          <h2>{{ quiz.title }}</h2>
-          <p>{{ quiz.questions.length }} Questions</p>
-          <button>Next</button>
-        </div>
-      </div>
+      <QuizCard v-else v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
     </section>
   </main>
 </template>
@@ -69,28 +63,5 @@ header {
   justify-content: center;
   flex-wrap: wrap;
   margin-top: 20px;
-}
-
-.card {
-  width: 270px;
-  margin: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  overflow: hidden;
-}
-
-.card img {
-  width: 100%;
-  height: 200px;
-  object-fit: cover;
-}
-
-.card-body {
-  padding: 0 10px;
-}
-
-h2 {
-  font-weight: bold;
 }
 </style>
---
+++ vuejs/quiz-vue/src/components/QuizCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="card">
+    <img :src="quiz.img" :alt="quiz.title">
+    <div class="card-body">
+      <h2>{{ quiz.title }}</h2>
+      <p>{{ quiz.questions.length }} Questions</p>
+      <button>Next</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 270px;
+  margin: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+}
+
+.card img {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+}
+
+.card-body {
+  padding: 0 10px;
+}
+
+h2 {
+  font-weight: bold;
+}
+</style>
