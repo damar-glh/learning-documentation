d893e133622ffaf06854550aa4a99f7575893449
Damar Galih <damarmuhamad13@gmail.com>
2024-10-19 21:54:48 +0700

    feat(nav): add active navlinks with property of routerlink

--- vuejs/learn-vue-router/src/components/Nav.vue
+++ vuejs/learn-vue-router/src/components/Nav.vue
@@ -1,15 +1,23 @@
 <script setup>
-import {RouterLink} from 'vue-router'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <div>
     <nav>
-      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/" exact-active-class="my-active-class">Home</RouterLink>
       |
-      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/about" exact-active-class="my-active-class">About</RouterLink>
       |
-      <RouterLink to="/contact">Contact</RouterLink>
+      <RouterLink to="/contact" exact-active-class="my-active-class">Contact</RouterLink>
     </nav>
   </div>
 </template>
+
+<style scoped>
+.my-active-class {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+</style>
--- vuejs/learn-vue-router/src/router/index.js
+++ vuejs/learn-vue-router/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../pages/Home.vue'
 import About from '../pages/About.vue'
 import Contact from "@/pages/Contact.vue";
