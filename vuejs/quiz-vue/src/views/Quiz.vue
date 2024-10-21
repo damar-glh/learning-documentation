dd72b5a0f62f0b0d43f2b15c8b28f9e524d8f14b
Damar Galih <damarmuhamad13@gmail.com>
2024-10-21 15:42:58 +0700

    feat(quiz): show data with props and indexing of json data

--- vuejs/quiz-vue/src/views/Quiz.vue
+++ vuejs/quiz-vue/src/views/Quiz.vue
@@ -1,3 +1,26 @@
+<script setup>
+import { useRoute } from "vue-router";
+import { ref } from "vue";
+import QuizHeader from "@/components/QuizHeader.vue";
+import QuizContent from "@/components/QuizContent.vue";
+import quizes from '@/data/quizes.json'
+
+const route = useRoute();
+const quiz = quizes.find((quiz) => quiz.id === parseInt(route.params.id));
+const currentIndexQuestion = ref(0)
+</script>
+
 <template>
-  <h1>Quiz Page</h1>
+  <div class="quiz">
+    <QuizHeader />
+    <QuizContent :question="quiz.questions[currentIndexQuestion]" />
+  </div>
 </template>
+
+<style scoped>
+.quiz {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+</style>
