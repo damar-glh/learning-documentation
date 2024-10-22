3d5ea7c19b7156d3d20a01031cb47125b32c82b1
Damar Galih <damarmuhamad13@gmail.com>
2024-10-22 21:32:47 +0700

    refactor(question): refactor watch with computed, better for data from ref

--- vuejs/quiz-vue/src/views/Quiz.vue
+++ vuejs/quiz-vue/src/views/Quiz.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useRoute } from "vue-router";
-import {ref, watch} from "vue";
+import {computed, ref, watch} from "vue";
 import QuizHeader from "@/components/QuizHeader.vue";
 import QuizContent from "@/components/QuizContent.vue";
 import quizes from '@/data/quizes.json'
@@ -9,14 +9,15 @@ const route = useRoute();
 const quiz = quizes.find((quiz) => quiz.id === parseInt(route.params.id));
 const currentIndexQuestion = ref(0);
 
-const questionPage = ref(`${currentIndexQuestion.value + 1}/${quiz.questions.length}`);
-
-watch(
-    () => currentIndexQuestion.value,
-    (currentIndexQuestion) => {
-        questionPage.value = `${currentIndexQuestion + 1}/${quiz.questions.length}`;
-    }
-)
+const questionPage = computed(() => `${currentIndexQuestion.value + 1}/${quiz.questions.length}`);
+// const questionPage = ref(`${currentIndexQuestion.value + 1}/${quiz.questions.length}`);
+//
+// watch(
+//     () => currentIndexQuestion.value,
+//     (currentIndexQuestion) => {
+//         questionPage.value = `${currentIndexQuestion + 1}/${quiz.questions.length}`;
+//     }
+// )
 </script>
 
 <template>
@@ -24,6 +25,7 @@ watch(
     <QuizHeader :questionPage="questionPage"/>
     <QuizContent :question="quiz.questions[currentIndexQuestion]" />
     <button @click="currentIndexQuestion++" :disabled="currentIndexQuestion === quiz.questions.length -1">Next</button>
+    <button @click="currentIndexQuestion--" :disabled="currentIndexQuestion === 0">Previous</button>
   </div>
 </template>
 
